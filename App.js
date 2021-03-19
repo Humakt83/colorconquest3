ae75e3c80cc5d6c74a53e93b276507e6af6c7bc2
Humakt83 <urlact@yahoo.com>
2021-03-19 16:49:58 +0200

    Stop playing music when app is inactive

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useRef} from 'react';
 
 import Menu from './src/components/Menu';
 import Board from './src/components/game/Board';
@@ -6,6 +6,7 @@ import Help from './src/components/Help';
 import Settings from './src/components/Settings';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
+import {AppState} from 'react-native';
 
 import SoundPlayer from 'react-native-sound-player';
 import {SettingsBus, EVENT_MUSIC, readSettings, SETTINGS_MUSIC_KEY} from './src/settings-util'
@@ -15,6 +16,8 @@ import Title from './src/components/Title';
 const Stack = createStackNavigator();
 
 const App: () => React$Node = () => {
+
+  const appState = useRef(AppState.currentState);
 
   let subscribedToMusic = null;
 
@@ -38,6 +41,22 @@ const App: () => React$Node = () => {
   })
 
   readSettings(SETTINGS_MUSIC_KEY, playMusic);
+
+  useEffect(() => {
+    AppState.addEventListener('change', handleAppStateChange);
+    return () => {
+      AppState.removeEventListener('change', handleAppStateChange);
+    }
+  });
+
+  const handleAppStateChange = (nextAppState) => {
+    if (appState.current.match(/inactive|background/) && nextAppState === 'active') {
+      readSettings(SETTINGS_MUSIC_KEY, playMusic);
+    } else if (appState.current === 'active' && nextAppState.match(/inactive|background/)) {
+      playMusic(false);
+    }
+    appState.current = nextAppState;
+  }
 
   const options = {headerTitle: () => <Title />};
 
